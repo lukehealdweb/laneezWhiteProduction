42ea06f7ffe5ba98cca16d9d06d4ee09d5323968
lukeuni <34680196+lukeuni@users.noreply.github.com>
2018-06-10 10:50:56 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+$(document).ready(function () {
+    "use strict";
+   $(window).scroll(function() {
+    if ($(window).scrollTop() > 10) {
+        $('#navBar').addClass('floatingNav');
+    } else {
+        $('#navBar').removeClass('floatingNav');
+    }
+});
+});
